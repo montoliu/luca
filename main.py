aaa2ad3a880ff1dc7a3faebd76a1370ac617f3a4
Raul Montoliu <montoliu@uji.es>
2023-10-21 16:31:04 +0200

    posi

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def read_log_file(filename):
     return posi, acce, gyro, magn, ahrs
 
 
-def plot_data(data, filename, series_names):
+def plot_data3D(data, filename, series_names):
     time = [float(entry[0]) for entry in data]
     x = [entry[1] for entry in data]
     y = [entry[2] for entry in data]
@@ -58,6 +58,7 @@ def plot_data(data, filename, series_names):
     ax3.legend()
 
     plt.savefig(filename)
+    plt.close(fig)
 
 
 def numerical_integration(time, values):
@@ -68,23 +69,23 @@ def numerical_integration(time, values):
     return integral
 
 
-def do_velocity(data):
-    time = [float(entry[0]) for entry in data]
-    x = [entry[1] for entry in data]
-    y = [entry[2] for entry in data]
-    z = [entry[3] for entry in data]
-    velo = []
-    velo.append([time[0], 0.0, 0.0, 0.0])
+def do_integration(data_in):
+    time = [float(entry[0]) for entry in data_in]
+    x = [entry[1] for entry in data_in]
+    y = [entry[2] for entry in data_in]
+    z = [entry[3] for entry in data_in]
+    data_out = []
+    data_out.append([time[0], 0.0, 0.0, 0.0])
 
     for i in range(1, len(time)):
         dt = time[i] - time[i - 1]
-        velo_x = velo[i - 1][1] + (x[i] + x[i - 1]) / 2 * dt
-        velo_y = velo[i - 1][2] + (y[i] + y[i - 1]) / 2 * dt
-        velo_z = velo[i - 1][3] + (z[i] + x[i - 1]) / 2 * dt
+        velo_x = data_out[i - 1][1] + (x[i] + x[i - 1]) / 2 * dt
+        velo_y = data_out[i - 1][2] + (y[i] + y[i - 1]) / 2 * dt
+        velo_z = data_out[i - 1][3] + (z[i] + x[i - 1]) / 2 * dt
 
-        velo.append([time[i], velo_x, velo_y, velo_z])
+        data_out.append([time[i], velo_x, velo_y, velo_z])
 
-    return velo
+    return data_out
 
 
 def do_remove_gravity(data):
@@ -115,42 +116,72 @@ def remove_first_seconds(data, seconds):
 
 
 if __name__ == '__main__':
-    posi_00, acce_00, gyro_00, magn_00, ahrs_00 = read_log_file("test_00.log")
-    plot_data(acce_00, "test_00_acce_row.png",  ['AcceX','AcceY','AcceZ'])
-    plot_data(ahrs_00, "test_00_ahrs_row.png",  ['PitchX', 'RollY', 'YawZ'])
-    velo_00 = do_velocity(acce_00)
-    plot_data(velo_00, "test_00_velo_row.png",  ['VeloX', 'VeloY', 'VeloZ'])
 
-    acce_nog_00 = do_remove_gravity(acce_00)
-    plot_data(acce_nog_00, "test_00_acce_nog.png", ['AcceX','AcceY','AcceZ'])
-    velo_nog_00 = do_velocity(acce_nog_00)
-    plot_data(velo_nog_00, "test_00_velo_nog.png",  ['VeloX', 'VeloY', 'VeloZ'])
+    l_tests = ['00', '15', 'tr']
 
-    posi_15, acce_15, gyro_15, magn_15, ahrs_15 = read_log_file("test_15.log")
-    plot_data(acce_15, "test_15_acce_row.png", ['AcceX','AcceY','AcceZ'])
-    plot_data(ahrs_15, "test_15_ahrs_row.png",  ['PitchX', 'RollY', 'YawZ'])
-    velo_15 = do_velocity(acce_15)
-    plot_data(velo_15, "test_15_velo_row.png",  ['VeloX', 'VeloY', 'VeloZ'])
+    for test in l_tests:
+        filename = "test_" + test + ".log"
+        print("Working with file: " + filename)
+        posi, acce, gyro_00, magn, ahrs = read_log_file(filename)
+        velo = do_integration(acce)                               # estimate velocity by integration
+        posi = do_integration(velo)                               # estimate position by integration
+        acce_nog = do_remove_gravity(acce)                        # remove gravity
+        velo_nog = do_integration(acce_nog)                       # estimate velocity by integration
+        posi_nog = do_integration(velo_nog)                       # estimate position by integration
 
-    acce_nog_15 = do_remove_gravity(acce_15)
-    plot_data(acce_nog_15, "test_15_acce_nog.png", ['AcceX','AcceY','AcceZ'])
-    velo_nog_15 = do_velocity(acce_nog_15)
-    plot_data(velo_nog_15, "test_15_velo_nog.png",  ['VeloX', 'VeloY', 'VeloZ'])
-
-    posi_tr, acce_tr, gyro_tr, magn_tr, ahrs_tr = read_log_file("track.log")
-    acce_tr = remove_first_seconds(acce_tr, 30)
-    gyro_tr = remove_first_seconds(gyro_tr, 30)
-    magn_tr = remove_first_seconds(magn_tr, 30)
-    ahrs_tr = remove_first_seconds(ahrs_tr, 30)
-
-    plot_data(acce_tr, "track_acce_row.png",  ['AcceX','AcceY','AcceZ'])
-    plot_data(ahrs_tr, "track_ahrs_row.png",  ['PitchX', 'RollY', 'YawZ'])
-    velo_tr = do_velocity(acce_tr)
-    plot_data(velo_tr, "track_velo_row.png",  ['VeloX', 'VeloY', 'VeloZ'])
-
-    acce_nog_tr = do_remove_gravity(acce_tr)
-    plot_data(acce_nog_tr, "track_acce_nog.png", ['AcceX','AcceY','AcceZ'])
-    velo_nog_tr = do_velocity(acce_nog_tr)
-    plot_data(velo_nog_tr, "track_velo_nog.png",  ['VeloX', 'VeloY', 'VeloZ'])
+        # Ploting
+        plot_data3D(acce, "test_" + test + "_acce_row.png", ['AcceX', 'AcceY', 'AcceZ'])
+        plot_data3D(ahrs, "test_" + test + "_ahrs_row.png", ['PitchX', 'RollY', 'YawZ'])
+        plot_data3D(velo, "test_" + test + "_velo_row.png", ['VeloX', 'VeloY', 'VeloZ'])
+        plot_data3D(posi, "test_" + test + "_posi_row.png", ['VeloX', 'VeloY', 'VeloZ'])
+        plot_data3D(acce_nog, "test_" + test + "_acce_nog.png", ['AcceX', 'AcceY', 'AcceZ'])
+        plot_data3D(velo_nog, "test_" + test + "_velo_nog.png", ['VeloX', 'VeloY', 'VeloZ'])
+        plot_data3D(posi_nog, "test_" + test + "_posi_nog.png", ['VeloX', 'VeloY', 'VeloZ'])
 
 
+    #     posi_00, acce_00, gyro_00, magn_00, ahrs_00 = read_log_file("test_00.log")
+    # velo_00 = do_integration(acce_00)             # estimate velocity by integration
+    # posi_00 = do_integration(velo_00)             # estimate position by integration
+    #
+    # acce_nog_00 = do_remove_gravity(acce_00)       # remove gravity
+    # velo_nog_00 = do_integration(acce_nog_00)      # estimate velocity by integration
+    # posi_nog_00 = do_integration(velo_00)         # estimate position by integration
+    #
+    # plot_data3D(acce_00, "test_00_acce_row.png",  ['AcceX','AcceY','AcceZ'])
+    # plot_data3D(ahrs_00, "test_00_ahrs_row.png",  ['PitchX', 'RollY', 'YawZ'])
+    # plot_data3D(velo_00, "test_00_velo_row.png",  ['VeloX', 'VeloY', 'VeloZ'])
+    # plot_data3D(posi_00, "test_00_posi_row.png", ['VeloX', 'VeloY', 'VeloZ'])
+    #
+    # plot_data3D(acce_nog_00, "test_00_acce_nog.png", ['AcceX','AcceY','AcceZ'])
+    # plot_data3D(velo_nog_00, "test_00_velo_nog.png",  ['VeloX', 'VeloY', 'VeloZ'])
+    # plot_data3D(posi_nog_00, "test_00_posi_row.png", ['VeloX', 'VeloY', 'VeloZ'])
+    #
+    #
+    # posi_15, acce_15, gyro_15, magn_15, ahrs_15 = read_log_file("test_15.log")
+    # plot_data3D(acce_15, "test_15_acce_row.png", ['AcceX','AcceY','AcceZ'])
+    # plot_data3D(ahrs_15, "test_15_ahrs_row.png",  ['PitchX', 'RollY', 'YawZ'])
+    # velo_15 = do_velocity(acce_15)
+    # plot_data3D(velo_15, "test_15_velo_row.png",  ['VeloX', 'VeloY', 'VeloZ'])
+    #
+    # acce_nog_15 = do_remove_gravity(acce_15)
+    # plot_data3D(acce_nog_15, "test_15_acce_nog.png", ['AcceX','AcceY','AcceZ'])
+    # velo_nog_15 = do_velocity(acce_nog_15)
+    # plot_data3D(velo_nog_15, "test_15_velo_nog.png",  ['VeloX', 'VeloY', 'VeloZ'])
+    #
+    # posi_tr, acce_tr, gyro_tr, magn_tr, ahrs_tr = read_log_file("test_tr.log")
+    # acce_tr = remove_first_seconds(acce_tr, 30)
+    # gyro_tr = remove_first_seconds(gyro_tr, 30)
+    # magn_tr = remove_first_seconds(magn_tr, 30)
+    # ahrs_tr = remove_first_seconds(ahrs_tr, 30)
+    #
+    # plot_data3D(acce_tr, "track_acce_row.png",  ['AcceX','AcceY','AcceZ'])
+    # plot_data3D(ahrs_tr, "track_ahrs_row.png",  ['PitchX', 'RollY', 'YawZ'])
+    # velo_tr = do_velocity(acce_tr)
+    # plot_data3D(velo_tr, "track_velo_row.png",  ['VeloX', 'VeloY', 'VeloZ'])
+    #
+    # acce_nog_tr = do_remove_gravity(acce_tr)
+    # plot_data3D(acce_nog_tr, "track_acce_nog.png", ['AcceX','AcceY','AcceZ'])
+    # velo_nog_tr = do_velocity(acce_nog_tr)
+    # plot_data3D(velo_nog_tr, "track_velo_nog.png",  ['VeloX', 'VeloY', 'VeloZ'])
+    #
+    #
